1e662b899a079ca5c02cd60cb9ed9356d9f34d7b
Mukama Martin <mukama.martin@gmail.com>
2015-02-10 16:04:53 +0300

    no message

--- MUST_Navigation/app/src/main/java/com/example/mukama/must_navigation/MapsActivity.java
+++ MUST_Navigation/app/src/main/java/com/example/mukama/must_navigation/MapsActivity.java
@@ -1,16 +1,28 @@
 package com.example.mukama.must_navigation;
 
+import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.hardware.Camera;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
-
+    // GPS Location
+    GPSTracker gps;
+    Boolean isConnection;
+    ConnectionDetector connection;
+    AlertDialogManager alert=new AlertDialogManager();
+    GoogleMapOptions options = new GoogleMapOptions();
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     //private MapView mapView;
     @Override
@@ -19,14 +31,8 @@ public class MapsActivity extends FragmentActivity {
         setContentView(R.layout.activity_maps);
         //mapView=(MapView) findViewById(R.id.map);
         //mapView.setBuiltInZoomControls(true);
+
         setUpMapIfNeeded();
-    }
-    public void onMapReady(GoogleMap map)
-    {
-        map.addMarker(new MarkerOptions()
-                .position( new LatLng(-0.615652, 30.656670))
-                .title("Marker")
-        );
     }
 
     @Override
@@ -56,8 +62,10 @@ public class MapsActivity extends FragmentActivity {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
+
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
+
                 setUpMap();
             }
         }
@@ -70,6 +78,26 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        connection=new ConnectionDetector(this);
+        gps=new GPSTracker(this);
+        isConnection=connection.isConnectingToInternet();
+        if(!isConnection){
+            alert.showAlertDialog(this,"Internet Setting","Check Internet Connection. Proceed to Settings", false);
+            return;
+        }
+        else {
+            if(gps.canGetLocation()) {
+                // the methods which are being applied getLatitude and getLongitude are being used to get the current location of the user
+               // mMap.getCameraPosition().zoom(13);
+                mMap.addMarker(new MarkerOptions().position(new LatLng(gps.getLatitude(), gps.getLongitude())).title("Marker"));
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                mMap.getUiSettings().isZoomControlsEnabled();
+
+               // CameraPosition cameraPosition=new CameraPosition.Builder().target(new LatLng(gps.getLatitude(), gps.getLongitude())).zoom(100).build();
+            }else{
+                alert.showAlertDialog(this,"GPS Setting","Please Enable Your GPS from Setting Such That We Locate Where You Are",false);
+                return;
+            }
+        }
     }
 }
