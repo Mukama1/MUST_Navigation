cb62e9461c1e54e6b43f0448fd7abd44ab5598d2
Mukama Martin <mukama.martin@gmail.com>
2015-04-03 08:39:22 +0300

    no message

---
+++ MUST_Navigation/app/src/main/java/com/example/mukama/must_navigation/StoreCoordinates.java
@@ -0,0 +1,18 @@
+package com.example.mukama.must_navigation;
+
+/**
+ * Created by Mukama on 3/20/2015.
+ */
+public class StoreCoordinates {
+    //cordinates for offices
+    public static final String[] officeCordinates={"-0.616510,30.656522","-0.617851,30.655879","-0.617088,30.656957"};
+    public static  final  String [] officeNames={"Dean of Students","MUST peer project offices","GUILD offices"};
+
+    //blocks and other departments coordinates
+    public static final String[] blockCordinates={"-0.614463,30.656973","-0.616502,30.656544","-0.616464,30.656930","-0.616679,30.657204","-0.616598,30.655932"};
+    public  static  final  String[] blockNames={"Faculty of developmental studies","Nursing department","Institute of computer science","Administrative block","Science block"};
+
+    //MUST canteens
+    public static  final String[] cateenCoordinates={"-0.614572	30.657740","-0.618178	30.656313"};
+    public static  final String[] canteenNames={"University canteen (DS faculty)","University canteen (behind ladies flat)"	};
+}
